11ae9cfc3a13306f261270825267a56bc457d478
Lionel Cassar <lionelcassar2501@gmail.com>
2021-09-09 16:10:29 -0300

    filtros blur y invert

--- app.js
+++ app.js
@@ -1,67 +1,58 @@
-/* CARGAR IMAGEN:
+/* BOTONES
+CARGAR IMAGEN:
 La funcion se llama cuando se seleciona un archivo.
 Creamos un variable que se asigna a un metodo javascript Image.
 Si la imagen esta corectamente cargar llama la funcion draw, asignada con la imagen
 en caso contrario a la funcion failed.
+La funcion failed se encarga de mostrar un mensaje de error 
  */
 
 document.getElementById('select_image').onchange = function(e) {
     var img = new Image();
-    var canvas = document.getElementById('canvas');
-    img.width = canvas.width
-    img.height = canvas.height
-    img.onload = draw;
-    img.onerror = failed;
     img.src = URL.createObjectURL(this.files[0]);
+    img.onload = function draw(){
+        var canvas = document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+    };
+    img.onerror = function failed(){
+        document.getElementById("error").innerHTML = "El archivo selecionado no se puede cargar";
+    };
 };
-
-/*
-La funcion draw se encarga de selecionar el canvas, adaptar el ancho y el alto, 
-*/
-
-function draw() {
-var canvas = document.getElementById('canvas');
-// // set size proportional to image
-// canvas.height = canvas.width * (img.height / img.width);
-
-// // step 1 - resize to 50%
-// var oc = document.createElement('canvas'),
-//     octx = oc.getContext('2d');
-
-// oc.width = img.width * 0.5;
-// oc.height = img.height * 0.5;
-// octx.drawImage(img, 0, 0, oc.width, oc.height);
-
-// // step 2
-// octx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5);
-
-// // step 3, resize to final size
-// ctx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5,
-// 0, 0, canvas.width, canvas.height);
-var ctx = canvas.getContext('2d');
-ctx.drawImage(this, 0,0);
-}
-
-/*
-La funcion failed se encarga de mostrar un mensaje de error 
-*/
-function failed() {
-document.getElementById("error").innerHTML = "El archivo selecionado no se puede cargar"
-}
-
 
 /* 
 Selecionamos el boton, le agregamos el event 'click'
 Convertimos el canvas en una imagen
 Creamos una variable que representa el canvas
 Creamos otra variable para dar una url al canvas
-Llamamos a la funcion de descarga con los parametros de la url y un titulo para la imagen */
+Llamamos a la funcion de descarga con los parametros de la url y un titulo para la imagen 
+*/
 
+document.querySelector('#btn-reset').addEventListener("click", function(e) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'blob:http://127.0.0.1:5500/627fd747-ea38-4c53-be55-b993f0b488bb', true);
+    xhr.responseType = 'blob';
+    xhr.onload = function(e) {
+    if (this.status == 200) {
+        var myBlob = this.response;
+        // myBlob is now the blob that the object URL pointed to.
+        console.log(myBlob)
+    }
+    };
+    xhr.send();
+});
+
+/*
+Selecionamos el boton, le agregamos el event 'click'
+Selecionamos el canvas
+usamos la funcion clearReact para dibujar un nuevo canvas, que en este caso sera vacio
+*/
 document.querySelector('#btn-save').addEventListener("click", function(e) {
     var canvas = document.querySelector('#canvas');
     var dataURL = canvas.toDataURL("image/jpeg", 1.0);
-
-    downloadImage(dataURL, 'my-canvas.jpeg');
+    downloadImage(dataURL, 'file.jpeg');
 });
 
 function downloadImage(data, filename = 'untitled.jpeg') {
@@ -72,11 +63,6 @@ function downloadImage(data, filename = 'untitled.jpeg') {
     a.click();
 }
 
-/*
-Selecionamos el boton, le agregamos el event 'click'
-Selecionamos el canvas
-usamos la funcion clearReact para dibujar un nuevo canvas, que en este caso sera vacio
-*/
 document.querySelector('#btn-new').addEventListener("click", function(e) {
     var canvas = document.querySelector('#canvas');
     var context = canvas.getContext('2d');
@@ -98,4 +84,72 @@ document.querySelector("#btn-standard").addEventListener("click", function(){
     canvas.style.width = "100%";
     canvas.style.margin = "0 0"
 })
+
+
+
+/* FILTROS*/ 
+document.getElementById('filtros').addEventListener('change', function() {
+    if(this.value == "brillo"){
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(canvas, 0, 0);
+        var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+        // invert colors
+        var i;
+        for (i = 0; i < imgData.data.length; i += 4) {
+            imgData.data[i] = 255 + imgData.data[i];
+            imgData.data[i+1] = 255 + imgData.data[i+1];
+            imgData.data[i+2] = 255 + imgData.data[i+2];
+            imgData.data[i+3] = 255;
+        }
+        ctx.putImageData(imgData, 0, 0);
+    } if(this.value == "negative"){
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(canvas, 0, 0);
+        var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+        //negative
+        var i;
+        for (i = 0; i < imgData.data.length; i += 4) {
+            imgData.data[i] = 255 - imgData.data[i];
+            imgData.data[i+1] = 255 - imgData.data[i+1];
+            imgData.data[i+2] = 255 - imgData.data[i+2];
+            imgData.data[i+3] = 255;
+        }
+        ctx.putImageData(imgData, 0, 0);
+    } if(this.value == "blur"){
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(canvas, 0, 0);
+        var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+        //blurr
+        var px = imgData.data;
+        var tmpPx = new Uint8ClampedArray(px.length);
+
+        for (var i = 0, len = px.length; i < len; i++) {
+            tmpPx[i] = px[i]
+        }
+        
+        for (var i = 0, len = px.length; i < len; i++) {
+            if(i % 3 === 4){
+                continue;
+            }
+            px[i] = (tmpPx[i]
+                + (tmpPx[i - 4] || tmpPx[i])
+                + (tmpPx[i + 4] || tmpPx[i])
+                + (tmpPx[i - 4 * imgData.width] || tmpPx[i])
+                + (tmpPx[i + 4 * imgData.width] || tmpPx[i])
+                + (tmpPx[i - 4 * imgData.width - 4] || tmpPx[i])
+                + (tmpPx[i + 4 * imgData.width + 4] || tmpPx[i])
+                + (tmpPx[i - 4 * imgData.width - 4] || tmpPx[i])
+                + (tmpPx[i + 4 * imgData.width + 4] || tmpPx[i])
+            ) / 9;
+        }
+        ctx.putImageData(imgData, 0, 0);
+        delete(tmpPx)
+    }
+  });
 
