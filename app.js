be3a09c8de951f5221c5060269df0e83b3fcdb3f
Francisco Vaninetti <francisco.vaninetti@asivas.com.ar>
2021-09-10 15:17:43 -0300

    refactor de app.js y agregado de filtros binario y sepia

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let canvas = document.querySelector("#canvas");
+    let ctx = canvas.getContext("2d");
+
+
 /* BOTONES
 CARGAR IMAGEN:
 La funcion se llama cuando se seleciona un archivo.
@@ -11,8 +16,8 @@ document.getElementById('select_image').onchange = function(e) {
     var img = new Image();
     img.src = URL.createObjectURL(this.files[0]);
     img.onload = function draw(){
-        var canvas = document.getElementById('canvas');
-        var ctx = canvas.getContext('2d');
+       // var canvas = document.getElementById('canvas');
+        //var ctx = canvas.getContext('2d');
         canvas.width = img.width;
         canvas.height = img.height;
         ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
@@ -49,8 +54,7 @@ Selecionamos el boton, le agregamos el event 'click'
 Selecionamos el canvas
 usamos la funcion clearReact para dibujar un nuevo canvas, que en este caso sera vacio
 */
-document.querySelector('#btn-save').addEventListener("click", function(e) {
-    var canvas = document.querySelector('#canvas');
+document.querySelector('#btn-save').addEventListener("click", function(e) {   
     var dataURL = canvas.toDataURL("image/jpeg", 1.0);
     downloadImage(dataURL, 'file.jpeg');
 });
@@ -63,28 +67,87 @@ function downloadImage(data, filename = 'untitled.jpeg') {
     a.click();
 }
 
-document.querySelector('#btn-new').addEventListener("click", function(e) {
-    var canvas = document.querySelector('#canvas');
-    var context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
+//Limpiamos el canvas utilizando clearRect.
+document.querySelector('#btn-new').addEventListener("click", function(e) {    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 })
 
 //OPCIONAL
-document.querySelector("#btn-small").addEventListener("click", function(e){
-    var canvas = document.querySelector('#canvas');
+document.querySelector("#btn-small").addEventListener("click", function(e){    
     canvas.style.width = "30%";
 })
 
-document.querySelector("#btn-medium").addEventListener("click", function(e){
-    var canvas = document.querySelector('#canvas');
+document.querySelector("#btn-medium").addEventListener("click", function(e){    
     canvas.style.width = "60%";
 })
-document.querySelector("#btn-standard").addEventListener("click", function(){
-    var canvas = document.querySelector('#canvas');
+document.querySelector("#btn-standard").addEventListener("click", function(){    
     canvas.style.width = "100%";
     canvas.style.margin = "0 0"
 })
 
+
+//Funciones de filtros
+function filterNegative()
+{
+    let img = ctx.getImageData(0,0,canvas.width,canvas.height);         
+        for(let x = 0; x < img.width; x++){
+            for(let y = 0; y < img.height; y++){
+                avgFilter = 3;
+                filterPixelNegative(img,x,y);
+                }
+        }  
+        ctx.putImageData(img,0,0);  
+}
+function filterPixelNegative(imgData,x,y)
+    {
+        let f = (x + y * imgData.width) * 4; 
+        //let avg = ( imgData.data[f + 0] + imgData.data[f + 1] + imgData.data[f + 2]) / avgFilter;       
+        imgData.data[f + 0] = 255 - imgData.data[f + 0];
+        imgData.data[f + 1] = 255 - imgData.data[f + 1];
+        imgData.data[f + 2] = 255 - imgData.data[f + 2];
+        imgData.data[f + 3] = 255; 
+    }
+
+    function filterSepia(){
+        let img = ctx.getImageData(0,0,canvas.width,canvas.height);
+        for(let x = 0; x < img.width; x++ ){
+            for(let y = 0; y < img.height; y++){
+                filterPixelSepia(img,x,y);
+            }
+        }
+        ctx.putImageData(img,0,0);
+    }
+    function filterPixelSepia(imgData,x,y){
+        let f = (x + y * imgData.width) * 4; 
+        let avg = ( imgData.data[f + 0] + imgData.data[f + 1] + imgData.data[f + 2]);       
+        imgData.data[f + 0] = avg * 0.46;
+        imgData.data[f + 1] = avg * 0.37;
+        imgData.data[f + 2] = avg * 0.17;
+    }
+
+    function filterBinary(){
+        let img = ctx.getImageData(0,0,canvas.width,canvas.height);
+        for(let x = 0; x < img.width; x++ ){
+            for(let y = 0; y < img.height; y++){
+                filterPixelBinary(img,x,y);
+            }
+        }
+        ctx.putImageData(img,0,0);
+    }
+
+    function filterPixelBinary(imgData,x,y){
+        let f = (x + y * imgData.width) * 4; 
+        let avg = ( imgData.data[f + 0] + imgData.data[f + 1] + imgData.data[f + 2]) / 3;      
+        if(avg < 127.5) {
+            imgData.data[f + 0] = 0;
+            imgData.data[f + 1] = 0;
+            imgData.data[f + 2] = 0;
+        }else{
+            imgData.data[f + 0] = 255;
+            imgData.data[f + 1] = 255;
+            imgData.data[f + 2] = 255;
+        }
+    }
 
 
 /* FILTROS*/ 
@@ -104,22 +167,17 @@ document.getElementById('filtros').addEventListener('change', function() {
             imgData.data[i+3] = 255;
         }
         ctx.putImageData(imgData, 0, 0);
-    } if(this.value == "negative"){
-        var canvas = document.getElementById("canvas");
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(canvas, 0, 0);
-        var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-        //negative
-        var i;
-        for (i = 0; i < imgData.data.length; i += 4) {
-            imgData.data[i] = 255 - imgData.data[i];
-            imgData.data[i+1] = 255 - imgData.data[i+1];
-            imgData.data[i+2] = 255 - imgData.data[i+2];
-            imgData.data[i+3] = 255;
-        }
-        ctx.putImageData(imgData, 0, 0);
-    } if(this.value == "blur"){
+    } 
+    if(this.value == "negative"){
+        filterNegative();
+    } 
+    if(this.value == "sepia"){
+        filterSepia();
+    }
+    if(this.value == "binary"){
+        filterBinary();
+    } 
+    if(this.value == "blur"){
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.drawImage(canvas, 0, 0);
@@ -152,4 +210,4 @@ document.getElementById('filtros').addEventListener('change', function() {
         delete(tmpPx)
     }
   });
-
+});
